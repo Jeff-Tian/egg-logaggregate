31e0a55ddbf7e63a01cc6c76354e9be21ad6877a
Jeff Tian <jie.tian@unicareer.com>
2018-12-28 12:22:56 +0800

    egg logaggregate plugin

---
+++ app.js
@@ -0,0 +1,27 @@
+const Transport = require("egg-logger").Transport;
+const fs = require("fs");
+
+module.exports = app => {
+  app.beforeStart(async () => {
+    app.getLogger("logger")
+      .set("aggregate", new AggregateTransport({ level: "INFO" }));
+
+    console.log("hello log aggregator");
+  });
+
+
+  class AggregateTransport extends Transport {
+    log(level, args, meta) {
+      fs.appendFileSync(app.config.logaggregate.path, JSON.stringify({
+        "@appname": app.config.logaggregate.appName,
+        "@env": process.env.NODE_ENV,
+        "@servername": meta.hostname,
+        "@timestamp": new Date(Date.now()),
+        level,
+        args,
+        meta
+      }) + "\n");
+    }
+  }
+
+};
---
+++ config/config.default.js
@@ -0,0 +1,4 @@
+exports.logaggregate = {
+  path: "logs/uni-academy-bff/uni-academy-bff-aggregate.json.log",
+  appName: "uni-academy-bff"
+};
