94e687f528e79e68aa1f4ba9d4778849e9d9ab58
Jeff Tian <jie.tian@unicareer.com>
2019-07-26 12:36:03 +0800

    :fire: 删除不需要的文件。

--- config/config.default.js
+++ config/config.default.js
@@ -22,51 +22,77 @@ module.exports = appInfo => {
     appInfo.name
   );
 
+  const common = () => ({
+    "@env": appInfo.env,
+    "@servername": os.hostname(),
+    "@timestamp": new Date(),
+    "@region": process.env.REGION || null,
+    "@serverip": networkAddress.address
+  });
+
+  const requestExtra = meta => ({
+    response: meta.ctx.body,
+    status: meta.ctx.status,
+    session: meta.ctx.session,
+    controller: meta.ctx.routerName,
+    request: {
+      params: meta.ctx.params,
+      body: meta.ctx.request.body
+    },
+    method: meta.ctx.method,
+    "@duration": meta.ctx.starttime ? Date.now() - meta.ctx.starttime : 0,
+    "@clientip": meta.ctx.headers["x-forwarded-for"]
+      ? meta.ctx.headers["x-forwarded-for"].split(",")[0]
+      : meta.ctx.ip,
+    url: meta.ctx.url,
+    "content-type": meta.ctx.headers["content-type"]
+  });
+
+  const filter = ({
+    controller,
+    request,
+    status,
+    url,
+    response,
+    "@region": region,
+    "@clientip": clientIp,
+    "@duration": duration,
+    "@env": env,
+    "@serverip": serverIp,
+    "@servername": serverName,
+    "@timestamp": timestamp,
+    "content-type": contentType,
+    method,
+    message,
+    level
+  }) => ({
+    controller,
+    request,
+    status,
+    url,
+    response,
+    "@region": region,
+    "@clientip": clientIp,
+    "@duration": duration,
+    "@env": env,
+    "@serverip": serverIp,
+    "@servername": serverName,
+    "@timestamp": timestamp,
+    "content-type": contentType,
+    method,
+    message,
+    level
+  });
   const formatter = meta =>
     JSON.stringify(
-      (({
-        controller,
-        request,
-        status,
-        url,
-        response,
-        "@region": region,
-        "@clientip": clientIp,
-        "@duration": duration,
-        "@env": env,
-        "@serverip": serverIp,
-        "@servername": serverName,
-        "@timestamp": timestamp,
-        "content-type": contentType,
-        method,
-        message,
-        level
-      }) => ({
-        controller,
-        request,
-        status,
-        url,
-        response,
-        "@region": region,
-        "@clientip": clientIp,
-        "@duration": duration,
-        "@env": env,
-        "@serverip": serverIp,
-        "@servername": serverName,
-        "@timestamp": timestamp,
-        "content-type": contentType,
-        method,
-        message,
-        level
-      }))({
-        "@env": appInfo.env,
-        "@servername": os.hostname(),
-        "@timestamp": new Date(),
-        "@region": process.env.REGION || null,
-        "@serverip": networkAddress.address,
+      filter({
+        ...common(),
         ...meta
       })
     );
+
+  const requestFormatter = meta =>
+    JSON.stringify({ ...common(), ...requestExtra(meta) });
 
   config.logger = {
     formatter,
@@ -81,7 +107,7 @@ module.exports = appInfo => {
   config.customLogger = {
     requestLogger: {
       file: path.join(logDir, "request.json.log"),
-      contextFormatter: formatter,
+      contextFormatter: requestFormatter,
       excludeUrls: ["/"]
     },
     scheduleLogger: {
